0e27435d5998ac71207ecb2a4545072817eaa8a0
madsemilf <55183287+Madsemilf@users.noreply.github.com>
2022-12-08 10:01:28 +0100

    made function for adding correct and wrong classes

--- js/modules/quiz.js
+++ js/modules/quiz.js
@@ -46,6 +46,15 @@ export default function Quiz() {
 		});
 	}
 
+	function setStatusClass(element, correct) {
+		clearStatusClass(element)
+		if (correct) {
+			element.classList.add('correct');
+		} else {
+			element.classList.add('wrong');
+		}
+	}
+
 	
 
 	/**
